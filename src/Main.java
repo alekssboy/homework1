9de607d868f485aa3dcfe45317d261a1100b7d34
alekssboy <aliaksandrbaiko1@gmail.com>
2023-11-11 13:32:59 +0300

    Branch1 BE: create new commit for test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-     int num =10;
-        for (int i = 0; i <num ; i++) {
+        int num = 10;
+        for (int i = 0; i < num; i++) {
             System.out.println(i);
             int r = 5;
-            while (r<=95){
+            while (r <= 95) {
                 System.out.println(r);
-               r=r+7;
+                r = r + 7;
             }
         }
-            }
-        }
+    }
+}
+// коммит с изменениями
